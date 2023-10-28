d2ec88d724f209eb66961b06a7e8adfc5a7b9214
EmilFerdinanA <emilakbar55@gmail.com>
2023-10-28 16:08:59 +0700

    responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,10 +8,11 @@ import Avatar from "@/assets/Avatar.png";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center px-28 py-5 border-b border-ternary">
+    <nav className="flex justify-between items-center px-6 sm:px-10 lg:px-28 py-5 border-b border-ternary">
       <div className="flex items-center gap-7">
         <Image src={CompanyLogo} alt={"company logo"} />
-        <ul className="flex gap-4 font-semibold">
+
+        <ul className="hidden gap-4 font-semibold lg:flex">
           <li>Dashboard</li>
           <li>CRM</li>
           <li>Submission</li>
@@ -20,10 +21,18 @@ const Navbar = () => {
         </ul>
       </div>
 
-      <div className="flex items-center gap-4">
-        <Image src={Seacrh} alt={"Seacrh"} />
-        <Image src={Setting} alt={"Setting"} />
-        <Image src={Bell} alt={"Bell"} />
+      <div className="flex gap-4">
+        <ul className="hidden lg:flex items-center gap-4">
+          <li>
+            <Image src={Seacrh} alt={"Seacrh"} />
+          </li>
+          <li>
+            <Image src={Bell} alt={"Bell"} />
+          </li>
+          <li>
+            <Image src={Setting} alt={"Setting"} />
+          </li>
+        </ul>
         <Image src={Avatar} alt={"Avatar"} />
       </div>
     </nav>
